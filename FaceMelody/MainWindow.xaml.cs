83d98b33b36b5e115312e498b98d7ba1ebc7232e
zhu-ty <zhu-ty@outlook.com>
2016-05-15 04:06:50 +0800

    done with read and save

--- FaceMelody/MainWindow.xaml.cs
+++ FaceMelody/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace FaceMelody
 {
@@ -27,7 +28,19 @@ namespace FaceMelody
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            SystemCore.SoundTools st = new SystemCore.SoundTools();
+            SystemCore.SoundTools.BaseSound bs = st.sound_reader("F:\\d多媒体技术及应用\\Program\\FaceMelody\\FaceMelody\\testwav.wav");
+
+            //bs = null;
+            //bs.clear();
+            bs.LVoice.RemoveRange(bs.LVoice.Count / 4, bs.LVoice.Count - bs.LVoice.Count / 4);
+            bs.RVoice.RemoveRange(bs.RVoice.Count / 4, bs.RVoice.Count - bs.RVoice.Count / 4);
+            st.sound_writer(bs,"F:\\d多媒体技术及应用\\Program\\FaceMelody\\FaceMelody\\testwavv.wav");
+            for (int i = 870; i < 890; i++)
+            {
+                Debug_Box.AppendText(bs.LVoice[i].ToString() + " ");
+            }
+            st = null;
         }
     }
 }
