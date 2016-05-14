a3b32952fb059ed756e09a3f73e69690ca47d805
zhu-ty <zhu-ty@outlook.com>
2016-05-15 00:28:56 +0800

    start working on tools

--- FaceMelody/SystemCore/SoundTools.cs
+++ FaceMelody/SystemCore/SoundTools.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
 namespace FaceMelody.SystemCore
 {
     class SoundTools
     {
+
+        public List<int> sound_reader(string file)
+        {
+            List<int> ret = new List<int>();
+            SoundPlayer sound_player = new SoundPlayer(file);
+            //sound_player.Stream
+            return ret;
+        }
     }
 }
