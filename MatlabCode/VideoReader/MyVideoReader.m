16e26cd165ad09165d0a2530522d1f99939f1708
zhu-ty <zhu-ty@outlook.com>
2016-05-31 17:50:20 +0800

    done with matlab audio seperate

--- MatlabCode/VideoReader/MyVideoReader.m
+++ MatlabCode/VideoReader/MyVideoReader.m
@@ -2,9 +2,7 @@ function MyVideoReader( filepath )
 %VIDEOREADER 读取视频并分离视频与音频轨道
 %   INPUT:
 %   filepath - 文件路径与文件名
-    audio_ = audioread(filepath);
-
-
-
+    [audio_,rate_] = audioread(filepath);
+    audiowrite('tmp_wavinfo_no_sync.wav',audio_,rate_);
 end
 
