b21ce925c956e9ab679f01d66469ad4e1618a017
lohasbai <liubaiby@126.com>
2016-06-01 02:02:59 +0800

    GUI in Timeline

--- FaceMelody/GUI/MainWorkshop.xaml.cs
+++ FaceMelody/GUI/MainWorkshop.xaml.cs
@@ -18,6 +18,8 @@ using System.Net.Sockets;
 using System.Diagnostics;
 using System.IO;
 
+using FaceMelody.SystemCore;
+
 namespace FaceMelody
 {
     /// <summary>
@@ -25,8 +27,14 @@ namespace FaceMelody
     /// </summary>
     public partial class MainWorkshop : Window
     {
+        public double fullTimeLength;
+
         public string[] materialList;
         public int materialNum;
+
+        public double videoLength;
+
+        public double curX;
 
         public bool isSelect;
         public bool isArea;
@@ -44,11 +52,23 @@ namespace FaceMelody
         {
             InitializeComponent();
 
+            fullTimeLength = 15*8.7*1000;
+
+            isPlay = false;
+            videoLength = 0;
+
             materialList = new string[15];
             materialNum = 0;
+
+            curX = 0;
+            DrawSelectBar();
+
+            isSelect = false;
             isArea = false;
+            isEffect = false;
+            isDelete = false;
+
             isDrawing = false;
-            isPlay = false;
         }
 
         #region Menu
@@ -63,6 +83,11 @@ namespace FaceMelody
                 if (filePath != "" || filePath != null)
                 {
                     this.VideoPlayer.Source = new Uri(filePath);
+
+                    this.VideoPlayer.Position = TimeSpan.FromMilliseconds(100);
+                    this.VideoPlayer.Play();
+                    Thread.Sleep(50);
+                    this.VideoPlayer.Pause();
                 }
             }
         }
@@ -115,23 +140,64 @@ namespace FaceMelody
             string sourceName = List_Label.Name;
 
             string path = materialList[Convert.ToInt16(sourceName.Substring(1, 1))];
-            MessageBox.Show(path);
+
+            AudioTools audioTool = new AudioTools();
+            AudioTools.BaseAudio baseAudio = audioTool.audio_reader(path);
+
+           /* FileStream fs = new FileStream("C:\\A.txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+            for (int i = 0; ; i++)
+            {
+                sw.Write(baseAudio.LVoice.ToString());
+                sw.Close();
+            }
+            fs.Close();
+               // MessageBox.Show(baseAudio.LVoice.Count.ToString());*/
         }
         #endregion
 
         #region Display
+
+        private void VideoPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            videoLength = VideoPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+            //MessageBox.Show(videoLength.ToString());
+            DrawVideoArea();
+        }
+
+        private void Display_Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Play_Image.Visibility = Visibility.Visible;
+        }
+
+        private void Display_Grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Play_Image.Visibility = Visibility.Hidden;
+        }
+
         private void Play_Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (!isPlay)
             {
                 isPlay = true;
                 this.VideoPlayer.Play();
+                Play_Image.Source = new BitmapImage(new Uri(@".\Icon\Pause_Icon.png", UriKind.Relative));
             }
             else
             {
                 isPlay = false;
                 this.VideoPlayer.Pause();
+                Play_Image.Source = new BitmapImage(new Uri(@".\Icon\Play_Icon.png", UriKind.Relative));
             }
+        }
+
+        private void VideoPlayer_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            isPlay = false;
+            this.VideoPlayer.Position = TimeSpan.FromMilliseconds(100);
+            this.VideoPlayer.Play();
+            Thread.Sleep(50);
+            this.VideoPlayer.Pause();
         }
         #endregion
 
@@ -146,6 +212,8 @@ namespace FaceMelody
             {
                 isSelect = true;
                 Select_Image.Source = new BitmapImage(new Uri(@".\Icon\SelectOn_Icon.png", UriKind.Relative));
+                isArea = false;
+                Area_Image.Source = new BitmapImage(new Uri(@".\Icon\AreaOff_Icon.png", UriKind.Relative));
             }
             else
             {
@@ -159,6 +227,8 @@ namespace FaceMelody
             {
                 isArea = true;
                 Area_Image.Source = new BitmapImage(new Uri(@".\Icon\AreaOn_Icon.png", UriKind.Relative));
+                isSelect = false;
+                Select_Image.Source = new BitmapImage(new Uri(@".\Icon\SelectOff_Icon.png", UriKind.Relative));
             }
             else
             {
@@ -169,19 +239,16 @@ namespace FaceMelody
 
         private void Effect_Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            this.VideoPlayer.Position += TimeSpan.FromSeconds(5);
-            this.VideoPlayer.Play();
-            Thread.Sleep(50);
-            this.VideoPlayer.Pause();
-
             if (!isEffect)
             {
                 isEffect = true;
+                Effect_Pop.IsOpen = true;
                 Effect_Image.Source = new BitmapImage(new Uri(@".\Icon\EffectOn_Icon.png", UriKind.Relative));
             }
             else
             {
                 isEffect = false;
+                Effect_Pop.IsOpen = false;
                 Effect_Image.Source = new BitmapImage(new Uri(@".\Icon\EffectOff_Icon.png", UriKind.Relative));
             }
         }
@@ -203,60 +270,141 @@ namespace FaceMelody
 
         #region Timeline
 
-        private void Timeline_Canvas_Click(object sender, MouseButtonEventArgs e)
+        private void Timeline_Area_Canvas_Click(object sender, MouseButtonEventArgs e)
         {
+            if (isSelect)
+            {
+                // Get current position of select bar
+                Point curPoint = e.GetPosition(this.Timeline_Area_Canvas);
+                curX = curPoint.X;
+                // Draw the current select bar
+                DrawSelectBar();
+                // Display the video at current position
+                double curTime = curX / 870 * fullTimeLength;
+                if (curTime > videoLength - 100)
+                {
+                    curTime = videoLength - 100;
+                }
+                this.VideoPlayer.Position = TimeSpan.FromMilliseconds(curTime);
+                this.VideoPlayer.Play();
+                Thread.Sleep(50);
+                this.VideoPlayer.Pause();
+                isPlay = false;
+                Play_Image.Source = new BitmapImage(new Uri(@".\Icon\Play_Icon.png", UriKind.Relative));
+            }
             if (isArea)
             {
-                Timeline_Canvas.Children.Clear();
-                Point startPoint = e.GetPosition(this.Timeline_Canvas);
+                //Get the position of start point of rectangle
+                Point startPoint = e.GetPosition(this.Timeline_Area_Canvas);
                 isDrawing = true;
                 startX = startPoint.X;
                 endX = startX;
             }
         }
 
-        private void Timeline_Canvas_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isDrawing && isArea)
-            {
-                Point endPoint = e.GetPosition(this.Timeline_Canvas);
-                endX = endPoint.X;
-                double width = endX - startX;
-                if (width < 0)
-                {
-                    width = -width;
-                }
-
-                Rectangle choiceArea = new Rectangle();
-                choiceArea.Width = width;
-                choiceArea.Height = 130;
-                if (endX - startX > 0)
-                {
-                    choiceArea.SetValue(Canvas.LeftProperty, startX);
-                }
-                else
-                {
-                    choiceArea.SetValue(Canvas.LeftProperty, endX);
-                }
-                SolidColorBrush myBrush = new SolidColorBrush(Colors.Green);
-                choiceArea.Fill = myBrush;
-                Timeline_Canvas.Children.Insert(0, choiceArea);
-            }
-            
-        }
-
-        private void Timeline_Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        private void Timeline_Area_Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
             if (isArea)
             {
+                //Get the position of end point of rectangle
+                Point endPoint = e.GetPosition(this.Timeline_Area_Canvas);
+                endX = endPoint.X;
                 isDrawing = false;
-                MessageBox.Show(startX.ToString() + "," + endX.ToString());
+                //Draw the rectangle of the chioce area
+                DrawChoiceArea();
+                //MessageBox.Show(startX.ToString() + "," + endX.ToString());
             }
         }
 
+        private void DrawSelectBar()
+        {
+            //Delete previous preSelectBar
+            Rectangle preSelectBar = Timeline_Area_Canvas.FindName("selectBar_Rectangle") as Rectangle;
+            if (preSelectBar != null)
+            {
+                Timeline_Area_Canvas.Children.Remove(preSelectBar);
+                Timeline_Area_Canvas.UnregisterName("selectBar_Rectangle");
+            }
+
+            //Draw new SelectBar
+            Rectangle selectBar = new Rectangle();
+            selectBar.Width = 2;
+            selectBar.Height = 130;
+            selectBar.SetValue(Canvas.LeftProperty, curX);
+            BrushConverter brushConverter = new BrushConverter();
+            Brush myBrush = (Brush)brushConverter.ConvertFromString("#80C0392B");
+            selectBar.Fill = myBrush;
+            Timeline_Area_Canvas.Children.Insert(0, selectBar);
+            Timeline_Area_Canvas.RegisterName("selectBar_Rectangle", selectBar);
+        }
+
+        private void DrawChoiceArea()
+        {
+            //Delete previous preSelectBar
+            Rectangle preChoiceArea = Timeline_Area_Canvas.FindName("choiceArea_Rectangle") as Rectangle;
+            if (preChoiceArea != null)
+            {
+                Timeline_Area_Canvas.Children.Remove(preChoiceArea);
+                Timeline_Area_Canvas.UnregisterName("choiceArea_Rectangle");
+            }
+
+            //Draw new video area
+            double width = endX - startX;
+            if (width < 0)
+            {
+                width = -width;
+            }
+
+            Rectangle choiceArea = new Rectangle();
+            choiceArea.Width = width;
+            choiceArea.Height = 110;
+            if (endX - startX > 0)
+            {
+                choiceArea.SetValue(Canvas.LeftProperty, startX);
+            }
+            else
+            {
+                choiceArea.SetValue(Canvas.LeftProperty, endX);
+            }
+            choiceArea.SetValue(Canvas.BottomProperty, 5.0);
+            BrushConverter brushConverter = new BrushConverter();
+            Brush myBrush = (Brush)brushConverter.ConvertFromString("#4027AE60");
+            choiceArea.Fill = myBrush;
+            Timeline_Area_Canvas.Children.Insert(0, choiceArea);
+            Timeline_Area_Canvas.RegisterName("choiceArea_Rectangle", choiceArea);
+        }
+
+        private void DrawVideoArea()
+        {
+            //Delete previous preSelectBar
+            Rectangle preVideoArea = Timeline_Area_Canvas.FindName("videoArea_Rectangle") as Rectangle;
+            if (preVideoArea != null)
+            {
+                Timeline_Area_Canvas.Children.Remove(preVideoArea);
+                Timeline_Area_Canvas.UnregisterName("videoArea_Rectangle");
+            }
+
+            //Draw new video area
+            double width = videoLength/fullTimeLength*870;
+
+            Rectangle videoArea = new Rectangle();
+            videoArea.Width = width;
+            videoArea.Height = 15;
+            videoArea.SetValue(Canvas.LeftProperty, 0.0);
+            videoArea.SetValue(Canvas.TopProperty, 10.0);
+
+            BrushConverter brushConverter = new BrushConverter();
+            Brush myBrush = (Brush)brushConverter.ConvertFromString("#40F39C12");
+            videoArea.Fill = myBrush;
+            Timeline_Area_Canvas.Children.Insert(0, videoArea);
+            Timeline_Area_Canvas.RegisterName("videoArea_Rectangle", videoArea);
+        }
+
+        private void DrawWave()
+        {
+
+        }
         #endregion
-
-
 
 
 
