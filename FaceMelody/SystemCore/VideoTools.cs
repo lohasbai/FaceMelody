fed70bc227482e35a5b35bd36924a49b7675cd84
zhu-ty <zhu-ty@outlook.com>
2016-05-31 20:21:32 +0800

    done with video_reader

--- FaceMelody/SystemCore/VideoTools.cs
+++ FaceMelody/SystemCore/VideoTools.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
-
+using System.IO;
 
 namespace FaceMelody.SystemCore
 {
     class VideoTools
     {
+        #region CONST
+        /// <summary>
+        /// 视频读取超时（毫秒数/10）
+        /// <para>分钟 * 60 * 100</para>
+        /// </summary>
+        const int TIME_OUT = 5 * 60 * 100;
+        #endregion
+
+
         #region BASE_STRUCT
         /// <summary>
         /// 基本视频类，包含一个视频文件名和一个音频
@@ -48,11 +57,38 @@ namespace FaceMelody.SystemCore
             ret.clear();
             try
             {
-                Process matlab = new Process();
+                if (!File.Exists(file))
+                    throw new Exception("找不到视频文件");
+                if (!File.Exists("MyVideoReader.exe"))
+                    throw new Exception("找到不到程序MyVideoReader.exe，请确保该可执行程序在主程序目录下");
+                Process matlab_pro = new Process();
+                ProcessStartInfo start_info = new ProcessStartInfo("MyVideoReader.exe", file);
+                matlab_pro.StartInfo = start_info;
+                matlab_pro.Start();
+                int i = 0;
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(10);
+                    i++;
+                    if (i > TIME_OUT)
+                    {
+                        matlab_pro.Kill();
+                        throw new Exception("程序运行超时");
+                    }
+                    if (matlab_pro.HasExited)
+                        break;
+                }
+                string wav_file = "tmp_wavinfo_no_sync.wav";
+                if (!File.Exists(wav_file))
+                    throw new Exception("生成的WAV文件未找到！请确保未手动删除！");
+                AudioTools at = new AudioTools();
+                ret.audio = at.audio_reader(wav_file);
+                File.Delete(wav_file);
                 ret.file = file;
             }
-            catch(Exception)
+            catch(Exception e)
             {
+                System.Windows.Forms.MessageBox.Show(e.Message);
                 ret.clear();
             }
             return ret;
